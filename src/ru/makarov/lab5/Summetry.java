faad362405fb3edfcd070e3774c2bd89b04b1c62
MakarovIlya <Makilval@gmail.com>
2013-02-05 20:44:34 +0700

    first commit

---
+++ src/ru/makarov/lab5/Summetry.java
@@ -0,0 +1,6 @@
+package ru.makarov.lab5;
+
+public interface Summetry {
+
+    void summet();
+}
---
+++ src/ru/makarov/lab5/TranslationByVektor.java
@@ -0,0 +1,6 @@
+package ru.makarov.lab5;
+
+public interface TranslationByVektor {
+
+    void transfer(Vektor p);
+}
